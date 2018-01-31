e50fe3143c893ab75243f24c6d24dced43c34243
Andrius <andriuscere@gmail.com>
2018-01-31 15:17:28 +0200

    Uzduotis0305

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+    static int suma;
+    static int[] mas = new int[5];
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite 5 skaicius");
+        for (int i = 0; i < 5; i++) {
+
+            int skaicius = sc.nextInt();
+            mas[i] = skaicius;
+            suma += skaicius;
+        }
+        System.out.println("suma " + isvestis());
+    }
+
+    public static int isvestis() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(mas[i]);
+        }
+        return suma;
+
+
     }
 }
